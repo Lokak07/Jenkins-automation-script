124b8c040ce948f49e1fbcaff23b88919f6c71fc
Lokak07 <lokashakthi19.ls@gmail.com>
2024-07-21 02:40:46 +0530

    final commit

---
+++ ec2/ec2.tf
@@ -0,0 +1,42 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "explore-california"
+
+  instance_type          = "t2.medium"
+  ami = var.ami
+  key_name               = "shakthi-manjunathan"
+  monitoring             = true
+  vpc_security_group_ids = [module.web_server_sg.security_group_id]
+  subnet_id = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+
+
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt install -y fontconfig openjdk-17-jre
+              if ! java -version; then
+                  echo "Java not installed properly"
+                  exit 1
+              fi
+              sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+              echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+              sudo apt-get update
+              sudo apt-get install -y jenkins
+              EOF
+
+}
+
+
+resource "aws_volume_attachment" "micro-attachment" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.jenkins-storage.id
+  instance_id = module.ec2_instance.id
+}
+
